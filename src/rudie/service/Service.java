42e893a09222a1c8a296aea21e9b5d31c6f295bc
Rudie de Smit <rddesmit@hotmail.com>
2014-07-20 18:09:40 +0200

    test met sqlite database connectie

--- src/rudie/service/Service.java
+++ src/rudie/service/Service.java
@@ -7,6 +7,8 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.StreamingOutput;
+import java.beans.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
@@ -18,13 +20,50 @@ import java.util.List;
 @Path("/service")
 public class Service {
 
+    Connection c = null;
+
+    public Service(){
+        try {
+            //maak connectie
+            Class.forName("org.sqlite.JDBC");
+            c = DriverManager.getConnection("jdbc:sqlite:test.db");
+            System.out.println("Opened database successfully");
+
+            //maak tabel
+            c.prepareStatement("CREATE TABLE test_table (test_date VARCHAR(250))").execute();
+            System.out.println("Table created");
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
     @GET
     @Path("{datetime}")
     @Produces("application/json")
     public StreamingOutput getItems(@PathParam("datetime")String datetime){
+        String databaseDatetime = null;
+
+        try {
+            //insert data
+            PreparedStatement statement = c.prepareStatement("INSERT INTO test_table VALUES (?)");
+            statement.setString(1, datetime);
+            statement.execute();
+            System.out.println("Data inserted");
+
+            //select data
+            statement = c.prepareStatement("SELECT * FROM test_table WHERE test_date = ?");
+            statement.setString(1, datetime);
+            ResultSet resultSet = statement.executeQuery();
+            resultSet.next();
+            databaseDatetime = resultSet.getString(1);
+            resultSet.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
         //item aanmaken
         HashMap<String, String> item = new HashMap<>();
-        item.put("id", datetime.toString());
+        item.put("id", databaseDatetime);
 
         //item in items list stoppen
         List<HashMap<String, String>> items = new ArrayList<>();
